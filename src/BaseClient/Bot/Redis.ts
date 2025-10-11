50bbf8a1ba57cfd2017fd45e5b5e34010396f513
Larsundso <Larsundso3@gmail.com>
2025-10-11 22:08:04 +0200

    feat: add caching for channel statuses, pins, and welcome screens

--- src/BaseClient/Bot/Redis.ts
+++ src/BaseClient/Bot/Redis.ts
@@ -4,6 +4,7 @@ import AuditLogCache from './CacheClasses/auditlog.js';
 import AutomodCache from './CacheClasses/automod.js';
 import BanCache from './CacheClasses/ban.js';
 import ChannelCache from './CacheClasses/channel.js';
+import ChannelStatusCache from './CacheClasses/channelStatus.js';
 import CommandCache from './CacheClasses/command.js';
 import CommandPermissionCache from './CacheClasses/commandPermission.js';
 import EmojiCache from './CacheClasses/emoji.js';
@@ -14,6 +15,7 @@ import IntegrationCache from './CacheClasses/integration.js';
 import InviteCache from './CacheClasses/invite.js';
 import MemberCache from './CacheClasses/member.js';
 import MessageCache from './CacheClasses/message.js';
+import PinCache from './CacheClasses/pin.js';
 import ReactionCache from './CacheClasses/reaction.js';
 import RoleCache from './CacheClasses/role.js';
 import SoundboardCache from './CacheClasses/soundboard.js';
@@ -24,6 +26,7 @@ import ThreadMemberCache from './CacheClasses/threadMember.js';
 import UserCache from './CacheClasses/user.js';
 import VoiceCache from './CacheClasses/voice.js';
 import WebhookCache from './CacheClasses/webhook.js';
+import WelcomeScreenCache from './CacheClasses/welcomeScreen.js';
 
 export const prefix = 'cache';
 const cacheDBnum = process.argv.includes('--dev') ? process.env.devCacheDB : process.env.cacheDB;
@@ -45,6 +48,7 @@ export const cache = {
  automods: new AutomodCache(cacheDB),
  bans: new BanCache(cacheDB),
  channels: new ChannelCache(cacheDB),
+ channelStatuses: new ChannelStatusCache(cacheDB),
  commands: new CommandCache(cacheDB),
  commandPermissions: new CommandPermissionCache(cacheDB),
  emojis: new EmojiCache(cacheDB),
@@ -55,6 +59,7 @@ export const cache = {
  invites: new InviteCache(cacheDB),
  members: new MemberCache(cacheDB),
  messages: new MessageCache(cacheDB),
+ pins: new PinCache(cacheDB),
  reactions: new ReactionCache(cacheDB),
  roles: new RoleCache(cacheDB),
  soundboards: new SoundboardCache(cacheDB),
@@ -65,4 +70,5 @@ export const cache = {
  users: new UserCache(cacheDB),
  voices: new VoiceCache(cacheDB),
  webhooks: new WebhookCache(cacheDB),
+ welcomeScreens: new WelcomeScreenCache(cacheDB),
 };
