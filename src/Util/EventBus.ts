e57636bd9c5e74be05f641b234f30743d36aad60
Larsundso <Larsundso3@gmail.com>
2025-10-12 15:48:45 +0200

    fix: remove data logging from event emission in debug mode

--- src/Util/EventBus.ts
+++ src/Util/EventBus.ts
@@ -4,7 +4,7 @@ import cacheDB from '../BaseClient/Bot/Redis.js';
 
 const emit = (type: GatewayDispatchEvents, data: GatewayDispatchPayload['d']) => {
  // eslint-disable-next-line no-console
- if (process.argv.includes('--debug')) console.log(`[EventBus] Emitting event: ${type}`, data);
+ if (process.argv.includes('--debug')) console.log(`[EventBus] Emitting event: ${type}`);
 
  cacheDB.publish(type, JSON.stringify(data));
 };
