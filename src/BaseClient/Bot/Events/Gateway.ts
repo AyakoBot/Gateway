da19dde3ab8d5db0a8818cb26a9ef3a8f59ca50e
larsundso <larsundso3@gmail.com>
2025-10-12 17:18:00 +0200

    refactor: enhance WebSocketShardEvents.Hello handler with shard management

--- src/BaseClient/Bot/Events/Gateway.ts
+++ src/BaseClient/Bot/Events/Gateway.ts
@@ -1,16 +1,25 @@
 /* eslint-disable no-console */
 import { WebSocketShardEvents } from '@discordjs/ws';
+import type { WebSocketManager } from '@discordjs/ws';
 import { GatewayOpcodes } from 'discord-api-types/v10';
 
 import cache from '../../../BaseClient/Bot/CacheHandlers/index.js';
 import { gateway } from '../Client.js';
 import Metrics from '../Metrics.js';
 
+// Type augmentation for internal WebSocketManager methods
+interface WebSocketManagerInternal extends WebSocketManager {
+ get(shardId: number): { setMaxListeners?: (n: number) => void } | undefined;
+}
+
 gateway.setMaxListeners(Object.keys(WebSocketShardEvents).length);
 
-gateway.on(WebSocketShardEvents.Hello, (shardId) =>
- console.log(`[Hello from Discord | Shard ${shardId}]`),
-);
+gateway.on(WebSocketShardEvents.Hello, (shardId) => {
+ const shard = (gateway as WebSocketManagerInternal).get(shardId);
+ if (shard?.setMaxListeners) shard.setMaxListeners(20);
+
+ console.log(`[Hello from Discord | Shard ${shardId}]`);
+});
 
 gateway.on(WebSocketShardEvents.Resumed, (shardId) =>
  console.log(`[Connection Resumed | Shard ${shardId}]`),
