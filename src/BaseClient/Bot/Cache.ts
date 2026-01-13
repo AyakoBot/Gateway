fac43b935a34f381dadd8e09246d9799921ef66c
Larsundso <Larsundso3@gmail.com>
2026-01-14 00:02:35 +0100

    Refactor code structure for improved readability and maintainability

--- src/BaseClient/Bot/Cache.ts
+++ src/BaseClient/Bot/Cache.ts
@@ -2,4 +2,4 @@ import { Cache } from '@ayako/utility';
 
 const cacheDBnum = process.argv.includes('--dev') ? process.env.devCacheDB : process.env.cacheDB;
 
-export default new Cache(Number(cacheDBnum));
+export default new Cache(Number(cacheDBnum), undefined, false);
--- src/BaseClient/Cluster/Manager.ts
+++ src/BaseClient/Cluster/Manager.ts
@@ -10,7 +10,7 @@ const manager = new ClusterManager('./dist/bot.js', {
  token: (process.argv.includes('--dev') ? process.env.DevToken : process.env.Token) ?? '',
  shardArgs: process.argv,
  execArgv: [
-  '--max-old-space-size=1024',
+  '--max-old-space-size=2048',
   '--experimental-json-modules',
   '--inspect=0.0.0.0:9229',
   ...(process.argv.includes('--dev') ? [] : ['--no-deprecation', '--no-warnings']),
