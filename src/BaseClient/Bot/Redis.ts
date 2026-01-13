d5c43299373087720c051baeac9e81833e957c62
Larsundso <Larsundso3@gmail.com>
2026-01-13 15:51:51 +0100

    move to diff repo

--- src/BaseClient/Bot/Redis.ts
+++
@@ -1,5 +0,0 @@
-import { Cache } from '@ayako/service/Classes/Cache.js';
-
-const cacheDBnum = process.argv.includes('--dev') ? process.env.devCacheDB : process.env.cacheDB;
-
-export default new Cache(Number(cacheDBnum));
