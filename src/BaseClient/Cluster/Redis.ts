371d4da9064156c7810c85df1bb8241333a33662
Lars_und_so <46791248+Larsundso@users.noreply.github.com>
2026-01-16 13:14:56 +0100

    refactor: replace BunRedisWrapper with createRedisWrapper for Redis connection

--- src/BaseClient/Cluster/Redis.ts
+++ src/BaseClient/Cluster/Redis.ts
@@ -1,11 +1,11 @@
-import { BunRedisWrapper } from '@ayako/utility';
+import { createRedisWrapper } from '@ayako/utility';
 
 const cacheDBnum = process.argv.includes('--dev') ? process.env.devCacheDB : process.env.cacheDB;
 if (!cacheDBnum && typeof cacheDBnum !== 'number') {
  throw new Error('No cache DB number provided in env vars');
 }
 
-export const cacheDB = new BunRedisWrapper({
+export const cacheDB = createRedisWrapper({
  host: process.argv.includes('--local') ? 'localhost' : 'redis',
  db: Number(cacheDBnum),
 });
