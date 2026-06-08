4a50aab54a7d0a23a101a1ea1290e65192e893a0
larsundso <larsundso3@gmail.com>
2026-06-08 23:46:47 +0200

    fix: Remove unnecessary newline in stats posting function

--- src/BaseClient/Cluster/Stats/DB.ts
+++ src/BaseClient/Cluster/Stats/DB.ts
@@ -18,7 +18,6 @@ export default ({
    // eslint-disable-next-line @typescript-eslint/naming-convention
    Authorization: process.env.DBToken ?? '',
   },
-
  })
   // eslint-disable-next-line no-console
   .then(async (r) => (r.ok ? undefined : console.log(await r.text())))
